325a5687552789b76342df92103883f9d354d457
ramonmelod <ramonmelo.com@gmail.com>
2024-03-07 14:40:47 -0300

    adição primeiros arquivos

---
+++ app.ts
@@ -0,0 +1,14 @@
+let n1 = 12;
+let str = "minha string";
+str = str.toUpperCase();
+let str2 = new String("minha string2");
+
+let a = 2;
+let b = 3;
+const c = a + b;
+str2.toUpperCase();
+console.log(n1, typeof n1);
+console.log(str);
+console.log(str2, typeof str2);
+console.log(str2 instanceof String);
+console.log(c);
